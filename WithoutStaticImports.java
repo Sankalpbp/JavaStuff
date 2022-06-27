7ee7674f327d33ecfcb1faaf32adf5ffe13419e7
Programmer <programmer@sankalp-ltmv64b.internal.salesforce.com>
2022-06-27 05:55:17 +0530

    Day 4

---
+++ WithoutStaticImports.java
@@ -0,0 +1,8 @@
+public class WithoutStaticImports {
+
+    public static void main (String [] args) {
+
+        System.out.println (Math.sqrt (4));
+        System.out.println (Math.random ());
+    }
+}
